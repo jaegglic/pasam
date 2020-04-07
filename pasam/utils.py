dbf7ce7e83e4da9991dae6d48d4335f250bc6fa8
jaegglic (insel) <christoph.jaeggli@insel.ch>
2020-04-07 17:27:11 +0200

    MAINT: Refactored module 'utils.py'

--- pasam/utils.py
+++ pasam/utils.py
@@ -22,33 +22,34 @@ import numpy as np
 import pasam._messages as msg
 from pasam._settings import NP_ORDER, NP_ATOL, NP_RTOL
 
-# TODO refactor this entire module
+
 # Public methods
 def ams_val_map_to_bool_map(vals):
     """Assigns `0` to ``True`` and `1` to ``False``.
 
     By default, the AMS generates files where the permitted nodes have
-    values `0` and the blocked nodes have value `1`.
+    values `0` and the blocked nodes have value `1` but for the module it is
+    more convenient to use `False` for blocked and `True` for permitted nodes.
     """
     # Value intervals for permitted (True) and blocked (False) nodes
-    INTERVAL_TRUE  = (-0.1, 0.1)
-    INTERVAL_FALSE = ( 0.9, 1.1)
+    INTVL_TRUE  = (-0.1, 0.1)
+    INTVL_FALSE = ( 0.9, 1.1)
 
-    # Reverse values
+    # Indices for reverse values
     vals = np.asarray(vals).ravel(order=NP_ORDER)
-    ind_true = np.logical_and(vals > INTERVAL_TRUE[0], vals < INTERVAL_TRUE[1])
-    ind_false = np.logical_and(vals > INTERVAL_FALSE[0], vals < INTERVAL_FALSE[1])
+    ind_true = np.logical_and(vals > INTVL_TRUE[0], vals < INTVL_TRUE[1])
+    ind_false = np.logical_and(vals > INTVL_FALSE[0], vals < INTVL_FALSE[1])
 
-    # Check whether all values have been covered
+    # Assign new values according to the intervals
     values = np.asarray(vals, dtype=bool)
-    if np.sum(np.logical_xor(ind_true, ind_false)) != len(values):
-        ind_not_unique = np.logical_not( np.logical_xor(ind_true, ind_false) )
-        ind = np.where(ind_not_unique)[0]
-        raise ValueError(msg.err0001(vals[ind]))
-
-    # Tag nodes according to the permission
     values[ind_true] = True
     values[ind_false] = False
+
+    # Check whether all values have been covered
+    if np.sum(np.logical_xor(ind_true, ind_false)) != len(values):
+        ind_not_unique = np.logical_not(np.logical_xor(ind_true, ind_false))
+        ind = np.where(ind_not_unique)[0]
+        raise ValueError(msg.err0001(vals[ind]))
 
     return values
 
@@ -57,7 +58,7 @@ def cartesian_product(*args, order='F'):
     """Cartesian product for a set of containers.
 
     Args:
-        args (array_like of array_like):
+        args (array_like of array_like): Input arguments to be composed.
         order (str {'C', 'F'}): 'F' for Fortran and 'C' for C-type ordering.
 
     Returns:
@@ -65,7 +66,7 @@ def cartesian_product(*args, order='F'):
 
     Notes:
         This function is equivalent to the function `itertools.product` but in
-        addiction let's the user choose the ordering (Fortran or C).
+        addiction the ordering can be chosen ('F' or 'C').
 
         For an input with two lists::
 
@@ -73,8 +74,14 @@ def cartesian_product(*args, order='F'):
 
         it produces a list of tuples such that::
 
-            [(a, e), (b, e), (c, e), (a, f), (b, f), (c, f)] (order='F')
-            [(a, e), (a, f), (b, e), (b, f), (c, e), (c, f)] (order='C').
+            order='F':
+            [(a, e), (b, e), (c, e),
+             (a, f), (b, f), (c, f)],
+
+            order='C':
+            [(a, e), (a, f),
+             (b, e), (b, f),
+             (c, e), (c, f)].
     """
     pools = map(list, args)
     result = [[]]
@@ -91,21 +98,19 @@ def cartesian_product(*args, order='F'):
 
 
 def within_conical_opening(center, distance, ratio, points):
-    """Indicates whether the points are within (True) or outside (False) of the
-    conical opening.
+    """Indicates whether the points are within (``True``) or outside
+    (``False``) of a conical opening.
 
-    Notes:
-
-        An example of a conical (symmetric) opening::
+    An example of a conical (symmetric) opening::
 
                       center
-                         *                ---
-                        / \                |
-                       /   \               |
-                      /     \              | distance
-                     /       \             |
-                    /         \            |
-                   /___________\          ---
+                         *                   ---
+                        / \                   |
+                       /   \                  |
+                      /     \                 | distance
+                     /       \                |
+                    /         \               |
+                   /___________\             ---
 
                   |-------------|            distance * ratio
 
@@ -113,10 +118,10 @@ def within_conical_opening(center, distance, ratio, points):
 
           [False,    True, True,   False]    return
 
-        where `distance` is the height of the triangle, the opening is the
-        product `distance * ratio` and `points` is a set of real values. For
-        the given example above, two out of four points are lying within the
-        conical (symmetric) opening.
+    where `distance` is the height of the triangle, the opening is the
+    product `distance * ratio` and `points` is a set of real values. For
+    the given example above, two out of four points are lying within the
+    conical (symmetric) opening.
 
     Args:
         center (float): Center of the cone.
@@ -133,15 +138,19 @@ def within_conical_opening(center, distance, ratio, points):
     v_min = center - v_range
     v_max = center + v_range
 
-    # Accept numerical inaccuracies; the below method is equivalent to
-    # ind = np.logical_or(ind, np.isclose(points, v_min, atol=NP_ATOL, rtol=NP_RTOL))
-    # ind = np.logical_or(ind, np.isclose(points, v_max, atol=NP_ATOL, rtol=NP_RTOL))
+    # Incorporate inaccuracies due to machine precision
     v_min -= NP_ATOL + NP_RTOL*abs(v_min)
     v_max += NP_ATOL + NP_RTOL*abs(v_max)
-
-    # Make standard less_equal and bigger_equal test
     points = np.asarray(points)
     ind = np.logical_and(points >= v_min, points <= v_max)
+
+    # NOTE The above method is equivalent to:
+    # points = np.asarray(points)
+    # ind = np.logical_and(points >= v_min, points <= v_max)
+    # ind = np.logical_or(ind, np.isclose(points, v_min,
+    #                                     atol=NP_ATOL, rtol=NP_RTOL))
+    # ind = np.logical_or(ind, np.isclose(points, v_max,
+    #                                     atol=NP_ATOL, rtol=NP_RTOL))
 
     return ind
 
@@ -155,27 +164,28 @@ def findall_num_in_str(s):
     Returns:
         list: List of numbers (`float` or `int`)
     """
-    re_num = r'-?[0-9]+\.?[0-9]*'
-    nums = re.findall(re_num, s)
+    # Looks for patterns similar to -56.37, 0.24, or 189
+    pat_num = r'-?[0-9]+\.?[0-9]*'
+    nums = re.findall(pat_num, s)
     return [_str2num(n) for n in nums]
 
 
-def isincreasing(vals, strict=True):
+def isincreasing(values, strict=True):
     """Checks if a set of values is increasing.
 
     Args:
-        vals (array_like, shape=(n,)): Set of values
+        values (array_like, shape=(n,)): Set of values
         strict (bool, optional): Strictly (`strict=True`) or simply
             (`strict=False`) increasing.
 
     Returns:
         bool
     """
-    vals = np.asarray(vals).ravel(order=NP_ORDER)
+    values = np.asarray(values).ravel(order=NP_ORDER)
     if strict:
-        return np.all(vals[:-1] < vals[1:])
+        return np.all(values[:-1] < values[1:])
     else:
-        return np.all(vals[:-1] <= vals[1:])
+        return np.all(values[:-1] <= values[1:])
 
 
 def readlines_(file, remove_blank_lines=False):
@@ -207,8 +217,8 @@ def readlines_(file, remove_blank_lines=False):
 def _isblank(s):
     """Check whether a string only contains whitespace characters.
     """
-    re_blank = r'^\s+$'
-    return bool(re.match(re_blank, s))
+    pat_blank = r'^\s+$'
+    return bool(re.match(pat_blank, s))
 
 
 def _str2num(s):
